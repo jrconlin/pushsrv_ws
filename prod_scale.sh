df61b15c3c84c8becd25c94f8704aaa9135b37cd
jrconlin <jconlin+git@mozilla.com>
2013-04-12 11:02:20 -0700

    Add simple script for bringing servers to scale

---
+++ prod_scale.sh
@@ -0,0 +1,29 @@
+# Run the following as root
+
+cat sysctl_settings.txt >> /sys/sysctl.conf
+
+# Increase the ipv4 port range:
+sysctl -w net.ipv4.ip_local_port_range="1024 65535"
+# General gigabit tuning:
+sysctl -w net.core.rmem_max=16777216
+sysctl -w net.core.wmem_max=16777216
+sysctl -w net.ipv4.tcp_rmem="4096 87380 16777216"
+sysctl -w net.ipv4.tcp_wmem="4096 65536 16777216"
+sysctl -w net.ipv4.tcp_syncookies=1
+# This gives the kernel more memory for tcp which you need with many (100k+) open socket connections
+sysctl -w net.ipv4.tcp_mem="50576   64768   98152"
+sysctl -w net.core.netdev_max_backlog=2500
+# This set the tcp max connections
+## sysctl -w net.netfilter.nf_conntrack_max=1233000
+ulimit -n 999999
+
+# modify /etc/security/limits.conf
+#  user soft nofile 50000
+#  user hard nofile 50000
+
+# modified /etc/sshd.conf
+# UsePrivilegeSeparation no
+
+# modified /usr/include/bits/typesizes.h
+
+# #define __FD_SETSIZE 65535
